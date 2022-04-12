f17efb527a008afb0d80fff7fe4dd87b955e78e3
Pavel-Bashkeev <pavelbashkeev@gmail.com>
2022-04-12 18:08:27 +0300

    fix

--- libraryApp/src/js/main.js
+++ libraryApp/src/js/main.js
@@ -2,50 +2,53 @@ import Navigo from 'navigo';
 import { animLogo } from './components/animLogo.js';
 import { search } from './components/search.js';
 
-  const library = document.querySelector('.library');
-  const book = document.querySelector('.book');
-  const addBook = document.querySelector('.book-added');
-  const btnsBack = document.querySelectorAll('.header-btn--back');
-  const btnsAddBook = document.querySelectorAll('.btn-add');
+const library = document.querySelector('.library');
+const book = document.querySelector('.book');
+const addBook = document.querySelector('.book-added');
+const btnsBack = document.querySelectorAll('.header-btn--back');
+const btnsAddBook = document.querySelectorAll('.btn-add');
 
-  // Anim logo
-  animLogo();
-  // Anim logo
- 
- 
-  const router = new Navigo('/', {
-    hash: true
-  })
-  router.on({
-    '': () => {
-      closeSection(library, book, addBook);
-      library.classList.remove('hidden');
+// Anim logo
+animLogo();
+// Anim logo
 
-    },
-    'book': () => {
+// search
+search();
+// search
 
-      closeSection(library, book, addBook);
-      book.classList.remove('hidden');
-    },
-    'add-book': () => {
-      closeSection(library, book, addBook);
-      addBook.classList.remove('hidden');
-    }
-  }).resolve()
-  
-  btnsBack.forEach(btn => {
-    btn.addEventListener('click', () => {
-      router.navigate('/')
-    })
+
+const router = new Navigo('/', {
+  hash: true
+})
+router.on({
+  '/': () => {
+    closeSection(library, book, addBook);
+    library.classList.remove('hidden');
+
+  },
+  'book': () => {
+
+    closeSection(library, book, addBook);
+    book.classList.remove('hidden');
+  },
+  'add-book': () => {
+    closeSection(library, book, addBook);
+    addBook.classList.remove('hidden');
+  }
+}).resolve()
+
+btnsBack.forEach(btn => {
+  btn.addEventListener('click', () => {
+    router.navigate('/')
   })
-  btnsAddBook.forEach(btn => {
-    btn.addEventListener('click', () => {
-      router.navigate('add-book')
-    })
+})
+btnsAddBook.forEach(btn => {
+  btn.addEventListener('click', () => {
+    router.navigate('add-book')
   })
-   // search
-   search();
-  // search
+})
+
+
 
 function closeSection(...sections) {
   sections.forEach(section => {
